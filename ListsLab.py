2a7319b578ebeebb5d618da372a482b188a925e3
MrsHines <34042849+MrsHines@users.noreply.github.com>
2018-11-13 14:35:27 -0600

    Starter File

---
+++ ListsLab.py
@@ -0,0 +1,4 @@
+# put your code here
+# You need a main() and at least one definition for each problem
+# You may add additional definitions if they will help you organize your code and be more efficient.
+# Do not forget to document your code with comments
